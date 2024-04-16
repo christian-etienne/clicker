8dad03f973183d1dec222fe63a183d876d046c72
michael <christian-michael.etienne@laplateforme.io>
2024-04-16 12:39:45 +0200

    ajout d'une fonction js compteur

---
+++ main.js
@@ -0,0 +1,13 @@
+
+      // Sélectionner l'élément du bouton
+      const clickButton = document.getElementById('clickButton');
+
+      // Initialiser le score
+      let score = 0;
+
+      // Mettre à jour le score lors du clic sur le bouton
+      clickButton.addEventListener('click', () => {
+          score++;
+          document.getElementById('score').textContent = score;
+      });
+    
